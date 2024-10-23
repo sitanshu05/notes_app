23696888fa72d8a7e097adaa6f66e9d2d66cab56
Sitanshu Hallad <83655509+sitanshu05@users.noreply.github.com>
2024-10-24 02:41:21 +0530

    appbar links added

--- packages/ui/src/AppBar/index.tsx
+++ packages/ui/src/AppBar/index.tsx
@@ -6,29 +6,52 @@ import Link from "next/link";
 import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/dropdown";
 import { Avatar } from "@nextui-org/avatar";
 import proifile_temp from "@assets/images/user_default.png"
+import { usePathname } from "next/navigation";
 
 export function NavBar({session}:any) {
 
+  const pathname = usePathname()
+
+  const isActive = (path: string) => pathname === path;
+
+
   return (
-    <Navbar>
+    <Navbar
+    classNames={{
+      item: [
+        "flex",
+        "relative",
+        "h-full",
+        "items-center",
+        "data-[active=true]:after:content-['']",
+        "data-[active=true]:after:absolute",
+        "data-[active=true]:after:bottom-0",
+        "data-[active=true]:after:left-0",
+        "data-[active=true]:after:right-0",
+        "data-[active=true]:after:h-[2px]",
+        "data-[active=true]:after:rounded-[2px]",
+        "data-[active=true]:after:bg-secondary",
+      ],
+    }}
+    >
       <NavbarBrand>
         <p className="font-bold text-inherit">NOTES</p>
       </NavbarBrand>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link className="text-foreground"  href="#">
+        <NavbarItem isActive={isActive("/courses")} className="data-[active]:text-secondary font-normal data-[active]:font-normal">
+          <Link href="/courses">
             Courses
           </Link>
         </NavbarItem>
-        <NavbarItem isActive >
-          <Link href="#" aria-current="page" className="text-secondary">
+        <NavbarItem isActive={isActive("/mynotes")} className="data-[active]:text-secondary font-normal data-[active]:font-normal">
+          <Link href="/mynotes" aria-current="page">
             My Notes
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link className="text-foreground" href="#">
-            Create Course
+          <Link className="text-gray-700" href="#">
+            Coming soon...
           </Link>
         </NavbarItem>
       </NavbarContent>
@@ -53,6 +76,10 @@ export function NavBar({session}:any) {
             </DropdownItem>
             <DropdownItem key="settings" on>My Settings</DropdownItem>
             <DropdownItem key="configurations">Configurations</DropdownItem>
+
+            <DropdownItem key="courses" className="sm:hidden sm:collapse" href="/courses"> Courses</DropdownItem>
+            <DropdownItem key="mynotes" className="sm:hidden sm:collapse" href="/mynotes">My Notes</DropdownItem>
+
             <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
             <DropdownItem key="logout" color="danger" onClick={()=>{signOut()}}>
               Log Out
