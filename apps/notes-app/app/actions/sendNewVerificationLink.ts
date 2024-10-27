cc9d396563ae12f573b470dcbdb6bba42b4c8000
Sitanshu Hallad <83655509+sitanshu05@users.noreply.github.com>
2024-10-28 02:36:26 +0530

    validation in verification action

--- apps/notes-app/app/actions/sendNewVerificationLink.ts
+++ apps/notes-app/app/actions/sendNewVerificationLink.ts
@@ -2,48 +2,60 @@
 import db from "@repo/db/client"
 import {transporter } from "../utils/emailTransporter";
 import { generateVerificationHashAndToken } from "../utils/generateVerificationHashAndToken";
-
+import z from "zod"
 
 export async function sendNewVerificationLink(email: string) {
 
-    const user = await db.user.findUnique({
-        where : {
-            email,
-            isVerified : false
-        },
-        include : {
-            verificationToken : true
+    try{
+        const emailSchema = z.string().email()
+        emailSchema.safeParse(email)
+    
+        const user = await db.user.findUnique({
+            where : {
+                email,
+                isVerified : false
+            },
+            include : {
+                verificationToken : true
+            }
+        })
+    
+        if (!user) {
+            // User not found or already verified
+            return;
         }
-    })
-
-    if (!user) {
-        // User not found or already verified
-        return;
+    
+        // Check if the current verification token has expired
+        const currentToken = user.verificationToken;
+        if (currentToken && currentToken.expiresAt > new Date()) {
+            // Token is still valid
+            return;
+        }
+    
+        const hashAndToken = generateVerificationHashAndToken();
+    
+        await db.verificationToken.update({
+            where : {
+                userId : user.id
+            },
+            data : {
+                token : hashAndToken.hash,
+                expiresAt : new Date(Date.now() + 1000 * 60 * 60 * 24),
+            }
+        })
+    
+        await transporter.sendMail({
+            from: process.env.EMAIL_ADDRESS,
+            to: email,
+            subject: "Verify your email",
+            html: `<a href="${process.env.NEXTAUTH_URL}/api/auth/verify?token=${hashAndToken.token}&userId=${user.id}">Verify</a>`
+        })
+    }catch(e  ){
+        return {
+            success : false,
+            error : "An error occured while sending the email verification link"
+        }
     }
 
-    // Check if the current verification token has expired
-    const currentToken = user.verificationToken;
-    if (currentToken && currentToken.expiresAt > new Date()) {
-        // Token is still valid
-        return;
-    }
-
-    const hashAndToken = generateVerificationHashAndToken();
-
-    await db.verificationToken.update({
-        where : {
-            userId : user.id
-        },
-        data : {
-            token : hashAndToken.hash,
-            expiresAt : new Date(Date.now() + 1000 * 60 * 60 * 24),
-        }
-    })
-
-    await transporter.sendMail({
-        from: process.env.EMAIL_ADDRESS,
-        to: email,
-        subject: "Verify your email",
-        html: `<a href="${process.env.NEXTAUTH_URL}/api/auth/verify?token=${hashAndToken.token}&userId=${user.id}">Verify</a>`
-    })
+   
 }
